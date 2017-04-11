b8155d6a276624480ac8fbc75a007c26c30c558c
vivaxy <xyxuye2007@126.com>
2017-04-11 19:44:45 +0800

    feat(First Commit): :tada:

---
+++ scripts/build.js
@@ -0,0 +1,8 @@
+/**
+ * @since 2017-04-11 19:33:08
+ * @author vivaxy
+ */
+
+require('babel-polyfill');
+require('babel-register')();
+require('./build/index');
---
+++ src/index.js
@@ -0,0 +1,40 @@
+/**
+ * @since 2017-04-11 19:28:05
+ * @author vivaxy
+ */
+
+import path from 'path';
+
+import glob from 'glob-promise';
+import fsp from 'fs-promise';
+import { transformFile } from 'babel-core';
+
+export default (SOURCE_PATH, BUILD_PATH) => {
+    const sourceFiles = path.join(SOURCE_PATH, `**`, `*.js`);
+    glob(sourceFiles)
+        .then((files) => {
+            const transformJobs = files.map((file) => {
+                return new Promise((resolve, reject) => {
+                    transformFile(file, (err, result) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            const relativeFilename = path.relative(SOURCE_PATH, file);
+                            resolve({
+                                relativeFilename,
+                                ...result,
+                            });
+                        }
+                    });
+                });
+            });
+            return Promise.all(transformJobs);
+        })
+        .then((results) => {
+            const writeJobs = results.map(({ relativeFilename, code }) => {
+                const outputFilename = path.join(BUILD_PATH, relativeFilename);
+                return fsp.outputFile(outputFilename, code);
+            });
+            return Promise.all(writeJobs);
+        });
+};
